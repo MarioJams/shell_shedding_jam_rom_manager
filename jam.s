da267794f5124347fea211e1d321212ca2cc8944
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2022-05-12 01:55:12 -0400

    Changed asm labels and binary files to use the original function names

--- jam.s
+++ jam.s
@@ -1,81 +1,81 @@
 .headersize 0x80400000 - 0x1200000
 .org 0x80408000
-func_0:
-.incbin "func_0.bin"
-func_1:
-.incbin "func_1.bin"
-func_2:
-.incbin "func_2.bin"
-func_3:
-.incbin "func_3.bin"
-func_4:
-.incbin "func_4.bin"
-func_5:
-.incbin "func_5.bin"
-func_6:
-.incbin "func_6.bin"
-func_7:
-.incbin "func_7.bin"
-func_8:
-.incbin "func_8.bin"
-func_9:
-.incbin "func_9.bin"
-func_10:
-.incbin "func_10.bin"
-func_11:
-.incbin "func_11.bin"
-func_12:
-.incbin "func_12.bin"
-func_13:
-.incbin "func_13.bin"
-func_14:
-.incbin "func_14.bin"
+act_riding_shell_ground_jam:
+.incbin "act_riding_shell_ground_jam.bin"
+act_riding_shell_air_jam:
+.incbin "act_riding_shell_air_jam.bin"
+act_water_shell_swimming_jam:
+.incbin "act_water_shell_swimming_jam.bin"
+act_hold_water_jump_jam:
+.incbin "act_hold_water_jump_jam.bin"
+check_water_jump_jam:
+.incbin "check_water_jump_jam.bin"
+update_swimming_speed_jam:
+.incbin "update_swimming_speed_jam.bin"
+update_shell_speed_jam:
+.incbin "update_shell_speed_jam.bin"
+set_water_plunge_action_jam:
+.incbin "set_water_plunge_action_jam.bin"
+perform_air_step_jam:
+.incbin "perform_air_step_jam.bin"
+bhv_koopa_shell_loop_jam:
+.incbin "bhv_koopa_shell_loop_jam.bin"
+squish_mario_model_jam:
+.incbin "squish_mario_model_jam.bin"
+perform_ground_quarter_step_jam:
+.incbin "perform_ground_quarter_step_jam.bin"
+perform_air_quarter_step_jam:
+.incbin "perform_air_quarter_step_jam.bin"
+perform_ground_step_jam:
+.incbin "perform_ground_step_jam.bin"
+check_common_submerged_cancels_jam:
+.incbin "check_common_submerged_cancels_jam.bin"
 
 .headersize 0x80246000 - 0x1000
 .org 0x80267504
-j func_0
+j act_riding_shell_ground_jam
 nop
 .org 0x8026bf40
-j func_1
+j act_riding_shell_air_jam
 nop
 .org 0x80272548
-j func_2
+j act_water_shell_swimming_jam
 nop
 .org 0x8026c738
-j func_3
+j act_hold_water_jump_jam
 nop
 .org 0x8027197c
-j func_4
+j check_water_jump_jam
 nop
 .org 0x80270c94
-j func_5
+j update_swimming_speed_jam
 nop
 .org 0x80264e18
-j func_6
+j update_shell_speed_jam
 nop
 .org 0x80253488
-j func_7
+j set_water_plunge_action_jam
 nop
 .org 0x80256b24
-j func_8
+j perform_air_step_jam
 nop
 .org 0x802bd680
-j func_9
+j bhv_koopa_shell_loop_jam
 nop
 .org 0x80253588
-j func_10
+j squish_mario_model_jam
 nop
 .org 0x80255b04
-j func_11
+j perform_ground_quarter_step_jam
 nop
 .org 0x802560ac
-j func_12
+j perform_air_quarter_step_jam
 nop
 .org 0x80255d88
-j func_13
+j perform_ground_step_jam
 nop
 .org 0x80274864
-j func_14
+j check_common_submerged_cancels_jam
 nop
 
 .orga 0xFD428
